cba861ef9633a2a0f2b8f27b6f27fb83cad0ae8b
pdkpavankumar <pavankumar8545@gmail.com>
2019-11-19 22:12:05 +0530

    some more changes

--- src/Add.js
+++ src/Add.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './common.css';
 import add from './plus.svg';
 
-const Add = () => (
-  <div className="addButton">
-    <img src={add} alt="add" className="add"/>
-  </div>
-);
+const Add = () => {
+  var addItem = () => {
+    //TODO: add
+  };
+  return (
+    <div className="addButton" onClick={addItem}>
+      <img src={add} alt="add" className="add" />
+    </div>
+  );
+}
 
 export default Add;
--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './common.css';
+import del from './delete.svg';
+import success from './success.svg';
+import verify from './verified.svg';
 
 const Todo = () => {
+  var done = true;
+  var removeItem = () => {
+    var remove = window.confirm('Are you sure want to delete');
+    if (remove) {
+      //TODO remove
+    }
+  };
 
+  var toggleItem = () => {
+    done = !done;
+  };
   return (
     <div className="task">
       <div className="input" contentEditable="true">
       </div>
-      
+      <div className="delete" onClick={removeItem}>
+        <img src={del} alt="delete" className="del" />
+      </div>
+      <div className="success" onClick={toggleItem}>
+        <img src={done ? success : verify} alt="checked" className="successicon" />
+      </div>
     </div>
   );
 }
