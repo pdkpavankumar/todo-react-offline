0430f23769899201ef852fd12a069322053390db
pdkpavankumar <pavankumar8545@gmail.com>
2019-11-20 22:26:19 +0530

    add full functionality

--- src/App.js
+++ src/App.js
@@ -14,10 +14,50 @@ class App extends React.Component {
 
   add = () => {
     this.setState({
-      todos: [{id: new Date().getTime(), text: ''}, ...this.state.todos]
+      todos: [{ id: new Date().getTime(), text: '', done: false }, ...this.state.todos]
     }, () => {
       localStorage.setItem('todos', JSON.stringify(this.state.todos));
-    });    
+    });
+  }
+
+  toggleItem = (id) => {
+    this.setState((prevProps) => {
+      return {
+        todos: prevProps.todos.map((item) => {
+          if (item.id === id) {
+            return { ...item, done: !item.done };
+          }
+          return item;
+        })
+      };
+    }, () => {
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    })
+  }
+
+  removeItem = (id) => {
+    this.setState((prevProps) => {
+      return {
+        todos: prevProps.todos.filter((item) => item.id !== id)
+      };
+    }, () => {
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    })
+  }
+
+  handleChange = (id, text = '') => {
+    this.setState((prevProps) => {
+      return {
+        todos: prevProps.todos.map((item) => {
+          if (item.id === id) {
+            return { ...item, text: text};
+          }
+          return item;
+        })
+      };
+    }, () => {
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    })
   }
 
   render() {
@@ -31,13 +71,13 @@ class App extends React.Component {
           this.state.todos.length === 0 ? (<h3>No Items Enjoy your day!</h3>) : this.state.todos.map((item) => {
             return (
               <div className="content paper" key={item.id}>
-                <Todo item={item} />
+                <Todo item={item} toggleItem={this.toggleItem} removeItem={this.removeItem} handleChange={this.handleChange} />
               </div>
             );
           })
 
         }
-        <Add add={this.add}/>
+        <Add add={this.add} />
         <div className="footer">
           TODO application offline....
         </div>
--- src/Todo.js
+++ src/Todo.js
@@ -5,28 +5,24 @@ import success from './success.svg';
 import verify from './verified.svg';
 
 class Todo extends React.Component {
+  constructor() {
+    super();
+    this.domnode = React.createRef();
+  }
+  shouldComponentUpdate(nextProps){
+    return nextProps.item.text !== this.domnode.current.innerHTML || this.props.item.done !== nextProps.item.done;
+  }
+
   render() {
-    var done = true;
-    var removeItem = () => {
-      var remove = window.confirm('Are you sure want to delete');
-      if (remove) {
-        //TODO remove
-      }
-    };
-
-    var toggleItem = () => {
-      done = !done;
-    };
-
     return (
       <div className="task">
-        <div className="input" contentEditable="true">
+        <div className="input" contentEditable="true" ref={this.domnode} dangerouslySetInnerHTML={{__html: this.props.item.text}} onInput={(event) => this.props.handleChange(this.props.item.id, event.target.innerHTML)}>
         </div>
-        <div className="delete" onClick={removeItem}>
+        <div className="delete" onClick={() => this.props.removeItem(this.props.item.id)}>
           <img src={del} alt="delete" className="del" />
         </div>
-        <div className="success" onClick={toggleItem}>
-          <img src={done ? success : verify} alt="checked" className="successicon" />
+        <div className="success" onClick={() => this.props.toggleItem(this.props.item.id)}>
+          <img src={this.props.item.done ? success : verify} alt="checked" className="successicon" />
         </div>
       </div>
     );
